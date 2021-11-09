fdcded762406265149294db9de642deb7d0ded6f
Santhosh Nageshwar <santhoshbabun@gmail.com>
2021-11-09 08:58:35 +0000

    adding pixel art test urls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+const TEST_PIXEL_ARTS = [
+	'https://avatars.dicebear.com/api/pixel-art/santhosh.svg',
+	'https://avatars.dicebear.com/api/pixel-art/ramya.svg',
+	'https://avatars.dicebear.com/api/pixel-art/meera.svg',
+	'https://avatars.dicebear.com/api/pixel-art/migan.svg'
+]
+
+
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -16,6 +26,7 @@ const App = () => {
           console.log('Phantom wallet is found');
           const response = await solana.connect({onlyIfTrusted: true});
           console.log("Connected with public key ", response.publicKey.toString());
+          setWalletAddress(response.publicKey.toString());
         } else {
           console.log('Please get a phantom wallet');
         }
@@ -27,10 +38,40 @@ const App = () => {
     }
   }
 
+  const connectWallet = async () => {
+    const {solana} = window;
+
+    if(solana) {
+      const response = await solana.connect();
+      console.log("Connected with public key ", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  }
+
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <div className="gif-grid">
+        {TEST_PIXEL_ARTS.map(art => (
+          <div className="gif-item" key={art}>
+            <img src={art} alt={art} />
+          </div>
+        ))}
+      </div> 
+    </div>
+  );
+
   useEffect(()=>{
-    window.addEventListener('load', async (event) => {
+   const onLoad = async () => {
       await checkIfWalletIsConnected();
-    });
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
   }, [])
 
   return (
@@ -41,12 +82,14 @@ const App = () => {
           <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
         </svg>
       </div>
-      <div className="container">
+      <div className={walletAddress? 'authed-container': 'container'}>
         <div className="header-container">
           <p className="header">PixelArt Portal</p>
           <p className="sub-text">
             View your Pixel Art collection in the metaverse based on the seed word✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
